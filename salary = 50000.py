01cc57b6353c6a132c90c0a1125076a421ee148e
vennalavannala0-hub <vennalavannala0@gmail.com>
2026-08-10 12:54:12 +0530

    Add files via upload

---
+++ salary = 50000.py
@@ -0,0 +1,10 @@
+salary = 50000.0
+
+salary += salary * 0.10
+print("Salary after Year 1:", salary)
+
+salary += salary * 0.10
+print("Salary after Year 2:", salary)
+
+salary += salary * 0.10
+print("Salary after Year 3:", salary)
